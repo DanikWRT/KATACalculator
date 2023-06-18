1028257bd4a6ceafb950374fe57a9e03c49a4a1b
DanikWRT <danil23004@gmail.com>
2023-06-18 20:47:31 +0700

    1.0

---
+++ KATACalculator/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("Введите математическую операцию между двумя числами от 1 до 10: ");
+        String mathOperation = scan.nextLine();
+        System.out.println(calc(mathOperation));
+        scan.close();
+    }
+    public static String calc(String answer) throws Exception {
+        String [] expression;
+        char operator = ' ';
+        for (int i = 0; i < answer.length(); i++) {
+            switch (answer.charAt(i)) {
+                case '*' -> operator = answer.charAt(i);
+                case '/' -> operator = answer.charAt(i);
+                case '+' -> operator = answer.charAt(i);
+                case '-' -> operator = answer.charAt(i);
+            }
+        }
+
+        System.out.println(operator);
+
+
+        return answer;
+    }
+}
